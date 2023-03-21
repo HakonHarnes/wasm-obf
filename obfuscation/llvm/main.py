196fe46355ae3b3d0515e3b93c283ddf0fe6ea36
Håkon Harnes <hakon@harnes.dev>
2023-03-21 15:41:57 +0100

    refactor: clean up llvm and add logging

--- obfuscation/llvm/main.py
+++ obfuscation/llvm/main.py
@@ -8,24 +8,26 @@ binary_path = f'{dir_path}/binaries/llvm'
 dataset_path = f'{dir_path}/dataset'
 
 transformations = [
-    {'desc': 'bcfobf', 'flags': '-mllvm -enable-bcfobf'},
-    {'desc': 'cffobf', 'flags': '-mllvm -enable-cffobf'},
-    {'desc': 'splitobf', 'flags': '-mllvm -enable-splitobf'},
-    {'desc': 'strcry', 'flags': '-mllvm -enable-strcry'},
-    {'desc': 'constenc', 'flags': '-mllvm -enable-constenc'},
-    {'desc': 'subobf', 'flags': '-mllvm -enable-subobf'},
-    {'desc': 'indibran', 'flags': '-mllvm -enable-indibran'},
-    {'desc': 'funcwra', 'flags': '-mllvm -enable-funcwra'},
-    {'desc': 'fco', 'flags': '-mllvm -enable-fco'},
-    {'desc': 'acdobf', 'flags': '-mllvm -enable-acdobf'},
-    {'desc': 'antihook', 'flags': '-mllvm -enable-antihook'},
-    {'desc': 'adb', 'flags': '-mllvm -enable-adb'},
+    '-mllvm -enable-bcfobf',
+    '-mllvm -enable-cffobf',
+    '-mllvm -enable-splitobf',
+    '-mllvm -enable-strcry',
+    '-mllvm -enable-constenc',
+    '-mllvm -enable-subobf',
+    '-mllvm -enable-indibran',
+    '-mllvm -enable-funcwra',
+    '-mllvm -enable-fco',
+    '-mllvm -enable-acdobf',
+    '-mllvm -enable-antihook',
+    '-mllvm -enable-adb',
 ]
 
 
 def get_emcc_out(path, transformation):
     program_name = path.split('/')[-1]
-    binary_name = f'{program_name}-{transformation}-llvm.html'
+    transformation_name = ''.join(
+        [x for x in transformation.split(' ') if x != '-mllvm'])
+    binary_name = f'{program_name}{transformation_name}-llvm.html'
     return os.path.join(binary_path, f'{binary_name}')
 
 
@@ -35,22 +37,30 @@ def get_file_in(path, transformation):
 
 
 def run_emcc(path, transformation):
-    emcc_out = get_emcc_out(path, transformation['desc'])
+
+    # get emscripten output file
+    emcc_out = get_emcc_out(path, transformation)
     if os.path.exists(emcc_out):
         return {'desc': f'Exists: {emcc_out}', 'code': 0}
 
-    file_in = get_file_in(path, transformation['desc'])
+    # get input file
+    file_in = get_file_in(path, transformation)
     if not os.path.exists(file_in):
         return {'desc': f'Missing input: {file_in}', 'code': 1}
 
+    # get build script
     script = os.path.join(path, 'build.sh')
     if not os.path.exists(script):
         return {'desc': f'Missing script: {script}', 'code': 1}
 
-    code = os.system(
-        f'/bin/sh {script} {file_in} {emcc_out} {transformation["flags"]} >/dev/null 2>&1')
+    # log file name
+    log_file = emcc_out.replace('.html', '.log')
 
-    return {'desc': f'Build: {emcc_out} {transformation["flags"]}', 'code': code}
+    # run build script
+    code = os.system(
+        f'/bin/sh {script} {file_in} {emcc_out} {transformation} > {log_file} 2>&1')
+
+    return {'desc': f'Build: {emcc_out}', 'code': code}
 
 
 def main():
