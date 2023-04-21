2b57d760e11869a54ada4e3e19c3ac2475f6fb6c
Håkon Harnes <hakon@harnes.dev>
2023-04-21 14:57:09 +0200

    feat(mongodb): create db dump command

---
+++ mongodb/dump-data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+database="wasm-obf"
+collections=("unobfuscated" "llvm" "tigress" "wasm-obf")
+
+# Create dump dir
+docker exec "mongodb" sh -c "mkdir -p /tmp/dump"
+
+# Dump data
+for collection in "${collections[@]}"; do
+  docker exec "mongodb" sh -c "mongoexport -d ${database} -c ${collection} --out /tmp/dump/${collection}.json --jsonArray --pretty"
+done
+
+# Copy dump to host
+docker cp "mongodb:/tmp/dump" .
