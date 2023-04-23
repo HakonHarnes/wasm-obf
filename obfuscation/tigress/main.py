d5934dfaabcb74ee5e06c829e150f5a5733b80fb
Håkon Harnes <hakon@harnes.dev>
2023-04-23 09:16:57 +0200

    feat(tigress): add tigress transformations for needleman_wunsch

--- obfuscation/tigress/main.py
+++ obfuscation/tigress/main.py
@@ -189,7 +189,7 @@ def obfuscate_documents(documents):
 
         # TODO: Remove
         # if document['category'] != 'games':
-        if document['name'] != 'lcs':
+        if document['name'] != 'needleman_wunsch':
             continue
 
         print_file(i + 1, len(documents), document['file'])
